bdacfce463b9f854e9a6d37709bf2a0b620f5549
Matu <matugaines@gmail.com>
2019-07-25 15:28:14 -0400

    Add examples from Object Oriented Programming chapter

---
+++ oop_init.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def say_hi(self):
+        print('Hello, my name is', self.name)
+
+
+p = Person('Swaroop')
+p.say_hi()
+# The previous two lines can also be written as
+# Person('Swaroop').say_hi()
+d = Person('Marshall')
+d.say_hi()
